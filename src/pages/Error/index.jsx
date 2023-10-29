f1eb6cd09fcfce62a0ee1454b7dc4ea7a52bb476
Tom Andre <tom.andre.dev@outlook.com>
2023-10-29 20:17:45 +0100

    Step 6 done, I have create the error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,30 +1,48 @@
 import { Link, useRouteError, isRouteErrorResponse } from 'react-router-dom'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 
 export default function ErrorPage() {
   let error = useRouteError()
-  console.log(error)
+
   return (
     <>
-      <section>
-        <h2>{error.status}</h2>
-        {error && isRouteErrorResponse(error) ? (
-          <>
-            {error.status === 404 && (
-              <div>Oups! La page que vous demandez n&apos;existe pas.</div>
-            )}
-            {error.status === 401 && (
-              <div>Oups! Vous n&apos;êtes pas autorisés à voir ça.</div>
-            )}
-            {error.status === 503 && (
-              <div>Oups! On dirait que notre API est en panne</div>
-            )}
-            {error.status === 418 && <div>🫖</div>}
-          </>
-        ) : (
-          <div>Oups! Quelque chose s&apos;est mal passé</div>
-        )}
-        <Link to="/accueil">Retourner sur la page d&apos;accueil</Link>
-      </section>
+      <div className="body-container">
+        <Header></Header>
+        <main className="main-errorPage">
+          <h2 className="errorPage__title">{error.status}</h2>
+          {error && isRouteErrorResponse(error) ? (
+            <>
+              {error.status === 404 && (
+                <div className="errorPage__msg fontSize-title">
+                  Oups! La page que vous demandez n&apos;existe pas.
+                </div>
+              )}
+              {error.status === 401 && (
+                <div className="errorPage__msg fontSize-title">
+                  Oups! Vous n&apos;êtes pas autorisés à voir ça.
+                </div>
+              )}
+              {error.status === 503 && (
+                <div className="errorPage__msg fontSize-title">
+                  Oups! On dirait que notre API est en panne
+                </div>
+              )}
+              {error.status === 418 && (
+                <div className="errorPage__msg fontSize-title">🫖</div>
+              )}
+            </>
+          ) : (
+            <div className="errorPage__msg fontSize-title">
+              Oups! Quelque chose s&apos;est mal passé
+            </div>
+          )}
+          <Link className="errorPage__link fontSize-paragraph" to="/accueil">
+            Retourner sur la page d&apos;accueil
+          </Link>
+        </main>
+      </div>
+      <Footer></Footer>
     </>
   )
 }
